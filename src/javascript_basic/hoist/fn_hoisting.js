91bf661fb3a0533981dcb8889c49308d97198040
michael Jo <csz0209@gmail.com>
2025-10-26 20:07:36 +0900

    ✍️study: 호이스팅 학습 완료

---
+++ src/javascript_basic/hoist/fn_hoisting.js
@@ -0,0 +1,7 @@
+// 함수 호이스팅
+connectStrings('study', 'hoisting');
+
+function connectStrings (str1, str2) {
+    console.log(str1 + str2);
+}
+
---
+++ src/javascript_basic/hoist/var_hosting.js
@@ -0,0 +1,13 @@
+// 변수 호이스팅
+
+// console.log(num1);
+// let num1 = 123;
+
+/**
+ * the way of JS's interpreting below code
+ * var num;
+ * console.log(num);
+ * num = 123;
+ */ 
+console.log(num2);
+var num2 = 123;
