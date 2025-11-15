4f650acd7b9aa1e1cc6be7a32334f4c3d267f1b9
michael Jo <csz0209@gmail.com>
2025-11-15 12:38:24 +0900

    ✍️study: DOM API-1 학습 완료
    desc: 텍스트 내용을 수정하려면 textContent 프로퍼티를 사용하자!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+//? innerHTML = HTML 태그를 해석하고 실행 → 👿"악성 코드"도 실행됨
+//? textContent = 모든 것을 단순 텍스트로 처리 →  ✅스크립트가 실행 안 됨
 
-const BDAY = '02-09';
+
+let $age = document.getElementById('age');
+$age.textContent = '5살';
+
+console.log($age);
+
+let $color = document.getElementById('color');
+$color.style.color = 'blue';
+$color.style.fontSize = '30px';
+
+
+
 
